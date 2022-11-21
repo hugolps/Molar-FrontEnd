5bd291152549bb3aaaa7f63b31633416d7bbfc38
João Evangelista <joaojorgeevangelista@gmail.com>
2022-11-21 20:40:47 -0300

    outros alerts

--- src/pages/catalogo-imoveis/edit/[id].js
+++ src/pages/catalogo-imoveis/edit/[id].js
@@ -1,8 +1,10 @@
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext, useEffect, forwardRef } from 'react'
 import { AuthContext } from 'src/contexts/AuthContext'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
 import {isFormValid, onlyNumbers, validate} from 'src/validation/index.js'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
@@ -40,10 +42,18 @@ const ResetButtonStyled = styled(Button)(({ theme }) => ({
   }
 }))
 
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const AdicionarImovel = () => {
   // ** States
-  const [openAlert, setOpenAlert] = useState(true)
   const [imgSrc, setImgSrc] = useState('/images/misc/triangle-dark.png')
+
+
+  const [openAlert, setOpenAlert] = useState(false)
+  const [messageAlert, setMessageAlert] = useState('')
+  const [severity, setSeverity] = useState('')
 
   const router = useRouter()
   const parametro = router.query.id
@@ -149,19 +159,43 @@ const AdicionarImovel = () => {
       body: JSON.stringify(updateValues)
     })
       .then(response => {
-          if(response.status === 200) {
-          response.json()
+        if(response.status === 200) {
+          return response.json()
           }
+
+          return Promise.reject(response)
         })
-      .then(data => router.push('/catalogo-imoveis'))
+
+
+        .then(data => {        setOpenAlert(true)
+          setMessageAlert('Imóvel editado com sucesso!')
+          setSeverity('primary')})
+      .then(data => {
+        setTimeout(() => router.push('/catalogo-imoveis'), 3000)
+      })
       .catch((error) => {
         console.log('Algo deu errado!', error)
+
+        setOpenAlert(true)
+        setMessageAlert('ERRO: Imóvel não editado. Tente novamente!')
+        setSeverity('error')
       })
   }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenAlert(false);
+    };
+
 
 
   return (
 
+
+  <>
   <CardContent>
         <Grid container spacing={7}>
           <Grid item xs={12} sm={6}>
@@ -313,6 +347,12 @@ const AdicionarImovel = () => {
           </Grid>
         </Grid>
     </CardContent>
+    <Snackbar open={openAlert} autoHideDuration={(severity === "error" ? 6000 : 3000)} onClose={handleClose}>
+    <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+    {messageAlert}
+    </Alert>
+    </Snackbar>
+    </>
   )
 }
 
--- src/pages/editImoveis/[id].js
+++ src/pages/editImoveis/[id].js
@@ -1,8 +1,10 @@
-import { useState, useContext, useEffect } from 'react'
+import { useState, useContext, useEffect, forwardRef } from 'react'
 import { AuthContext } from 'src/contexts/AuthContext'
 import Cookies from 'js-cookie'
 import { useRouter } from 'next/router'
 import {isFormValid, onlyNumbers, validate} from 'src/validation/index.js'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
@@ -41,10 +43,17 @@ const ResetButtonStyled = styled(Button)(({ theme }) => ({
   }
 }))
 
+const Alert = forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 const AdicionarImovel = () => {
   // ** States
-  const [openAlert, setOpenAlert] = useState(true)
   const [imgSrc, setImgSrc] = useState('/images/misc/triangle-dark.png')
+
+  const [openAlert, setOpenAlert] = useState(false)
+  const [messageAlert, setMessageAlert] = useState('')
+  const [severity, setSeverity] = useState('')
 
   const router = useRouter()
   const parametro = router.query.id
@@ -154,22 +163,43 @@ const AdicionarImovel = () => {
       body: JSON.stringify(updateValues)
     })
       .then(response => {
-          if(response.status === 200) {
-          response.json()
+        if(response.status === 200) {
+          return response.json()
           }
+
+          return Promise.reject(response)
         })
       // .then(data => setUserInfo({
       //   usuario: updateValues.usuario,
       //   Authorization: userInfo.Authorization
       // }))
-      .then(data => router.push('/'))
+      .then(data => {
+        setOpenAlert(true)
+        setMessageAlert('Imóvel editado com sucesso!')
+        setSeverity('primary')
+      })
+      .then(data => {
+        setTimeout(() => router.push('/'), 3000)
+      })
       .catch((error) => {
         console.log('Algo deu errado!', error)
+        setOpenAlert(true)
+        setMessageAlert('ERRO: Imóvel não editado. Tente novamente!')
+        setSeverity('error')
       })
   }
 
-  return (
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
 
+    setOpenAlert(false);
+    };
+
+
+  return (
+  <>
   <CardContent>
         <Grid container spacing={7}>
           <Grid item xs={12} sm={6}>
@@ -269,6 +299,12 @@ const AdicionarImovel = () => {
           </Grid>
         </Grid>
     </CardContent>
+    <Snackbar open={openAlert} autoHideDuration={(severity === "error" ? 6000 : 3000)} onClose={handleClose}>
+    <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }}>
+    {messageAlert}
+    </Alert>
+    </Snackbar>
+    </>
   )
 }
 
